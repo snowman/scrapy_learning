f8f5ab5a2f7072f4d6219660ed5338068ef05c9e
snowman <unmem@outlook.com>
2021-11-27 23:10:52 +0800

    update pipelines.py (add MongoDBPipeline)

--- demo/demo/pipelines.py
+++ demo/demo/pipelines.py
@@ -59,3 +59,26 @@ class DuplicatesPipeline(object):
         self.book_set.add(name)
 
         return item
+
+# mongodb
+from scrapy.item import Item
+import pymongo
+
+class MongoDBPipeline(object):
+    DB_URI = 'mongodb://localhost:27017/'
+    DB_NAME = 'scrapy_data'
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.DB_URI)
+        self.db = self.client[self.DB_NAME]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        collection = self.db[spider.name]
+        post = dict(item) if isinstance(item, Item) else item
+
+        collection.insert_one(post)
+
+        return item
