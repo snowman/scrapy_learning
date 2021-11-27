caa302607123905bb4f8bbf305bad8129557da6f
snowman <unmem@outlook.com>
2021-11-27 20:40:45 +0800

    update book_spider.py (use items.py model)

--- demo/demo/items.py
+++ demo/demo/items.py
@@ -10,3 +10,48 @@ class DemoItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+# 使用 Python 字典 存储 一本书 的 信息 有 以下 缺点:
+#
+# 1. 无法一目了然地了解数据中包含哪些字段, 影响代码可读性
+# 2. 缺乏对字段名字的检测, 容易因程序员的笔误而出错
+# 3. 不便于携带元数据(传递给其他组件的信息)
+#
+# 为 解决 上述 问题, 在 Scrapy 中 可以 使用 自定义 的 Item 类 封装
+
+from scrapy import Item, Field
+class BookItem(Item):
+    name = Field()
+    price = Field()
+
+# >>> book1 = BookItem(name='Needful Things', price=45.0)
+# >>> book1
+# {'name': 'Needful Things', 'price': 45.0}
+#
+# >>> book2 = BookItem()
+# >>> book2
+# {}
+#
+# >>> book2['name'] = 'Life of Pi'
+# >>> book2['price'] = 32.5
+# >>> book2
+# {'name': 'Life of Pi', 'price': 32.5}
+
+# prevent typo
+#
+# >>> book = BookItem()
+# >>> book['name'] = 'Memoirs of a Geisha'
+# >>> book['prize'] = 43.0 # typo
+# Traceback (most recent call last):
+# ...
+# KeyError: 'BookItem does not support field: prize'
+
+# acess
+#
+# >>> book = BookItem(name='Needful Things', price=45.0)
+# >>> book['name']
+# 'Needful Things'
+# >>> book.get('price', 60.0)
+# 45.0
+# >>> list(book.items())
+# [('price', 45.0), ('name', 'Needful Things')]
--- demo/demo/spiders/book_spider.py
+++ demo/demo/spiders/book_spider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from ..items import BookItem
 
 
 class BooksSpider(scrapy.Spider):
@@ -13,19 +14,17 @@ class BooksSpider(scrapy.Spider):
         # 提取数据
         # 每一本书的信息在<article class="product_pod">中, 我们使用
         # css()方法找到所有这样的 article 元素, 并依次迭代
-        for book in response.css("article.product_pod"):
+        for sel in response.css("article.product_pod"):
+            book = BookItem()
             # 书名信息在 article > h3 > a 元素的 title 属性里
             # 例如: <a title="A Light in the Attic">A Light in the ...</a>
-            name = book.xpath("./h3/a/@title").extract_first()
+            book['name'] = sel.xpath("./h3/a/@title").extract_first()
 
             # 书价信息在 <p class="price_color">的 TEXT 中.
             # 例如: <p class="price_color">£51.77</p>
-            price = book.css("p.price_color::text").extract_first()
+            book['price'] = sel.css("p.price_color::text").extract_first()
 
-            yield {
-                "name": name,
-                "price": price,
-            }
+            yield book
 
         # 提取链接
         # 下一页的 url 在 ul.pager > li.next > a 里面
