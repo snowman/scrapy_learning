f4cab7a35c13213cda80b71367664b9674bf36fd
snowman <unmem@outlook.com>
2021-11-28 22:17:23 +0800

    toscrape_book: update items.py (add BookDetailItem)

--- toscrape_book/toscrape_book/items.py
+++ toscrape_book/toscrape_book/items.py
@@ -10,3 +10,11 @@ class ToscrapeBookItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class BookDetailItem(scrapy.Item):
+    name          = scrapy.Field()
+    price         = scrapy.Field()
+    review_rating = scrapy.Field() # range from [1, 5]
+    review_num    = scrapy.Field()
+    upc           = scrapy.Field() # Universal Product Code
+    stock         = scrapy.Field()
