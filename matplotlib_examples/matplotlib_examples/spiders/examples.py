b19113499b8b590c3d8574c3647e4f731b0655fb
snowman <unmem@outlook.com>
2021-11-29 09:21:00 +0800

    matplotlib_examples: update spider (examples)

--- matplotlib_examples/matplotlib_examples/spiders/examples.py
+++ matplotlib_examples/matplotlib_examples/spiders/examples.py
@@ -1,10 +1,29 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from ..items import ExampleItem
 
 
 class ExamplesSpider(scrapy.Spider):
     name = 'examples'
     allowed_domains = ['matplotlib.org']
-    start_urls = ['http://matplotlib.org/']
+    start_urls = ['http://matplotlib.org/examples/index.html']
 
     def parse(self, response):
-        pass
+        # or restrict_css='div.toctree-wrapper.compound li.toctree-l2' without deny parameter
+        le = LinkExtractor(restrict_css='div.toctree-wrapper.compound',
+                           deny='/index.html$')
+
+        print("Total example files: {0}".format(len(le.extract_links(response))))
+
+        for link in le.extract_links(response):
+            yield scrapy.Request(link.url, callback=self.parse_example)
+
+    def parse_example(self, response):
+        example = ExampleItem()
+
+        href = response.css('a.reference.external::attr(href)').extract_first()
+        url = response.urljoin(href)
+
+        example['file_urls'] = [url]
+
+        yield example
