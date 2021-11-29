cf8e38936b0a9ce326525c68ae944cce34542715
snowman <unmem@outlook.com>
2021-11-29 08:48:46 +0800

    matplotlib_examples: add spider (examples)
    $ scrapy genspider examples matplotlib.org

---
+++ matplotlib_examples/matplotlib_examples/spiders/examples.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class ExamplesSpider(scrapy.Spider):
+    name = 'examples'
+    allowed_domains = ['matplotlib.org']
+    start_urls = ['http://matplotlib.org/']
+
+    def parse(self, response):
+        pass
