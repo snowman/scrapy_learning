4c62cb109aacbd87e355636c81ed9d9253b9ec74
snowman <unmem@outlook.com>
2021-11-29 15:48:25 +0800

    so_images: update spider (images)

--- so_images/so_images/spiders/images.py
+++ so_images/so_images/spiders/images.py
@@ -1,10 +1,30 @@
 import scrapy
+from scrapy import Request
+from ..items import ImageItem
+import json
 
 
 class ImagesSpider(scrapy.Spider):
+    BASE_URL = 'https://image.so.com/zjl?ch=art&sn=%s'
+    start_index = 0
+
+    # prevent consuming huge disk
+    MAX_DOWNLOAD_NUM = 1000
+
     name = 'images'
     allowed_domains = ['image.so.com']
-    start_urls = ['http://image.so.com/']
+    start_urls = [BASE_URL % 0]
 
     def parse(self, response):
-        pass
+        image = ImageItem()
+
+        infos = json.loads(response.body.decode('utf-8'))
+
+        image['image_urls'] = [info['qhimg_url'] for info in infos['list']]
+
+        yield image
+
+        self.start_index += infos['count']
+
+        if infos['count'] > 0 and self.start_index < self.MAX_DOWNLOAD_NUM:
+            yield Request(self.BASE_URL % self.start_index)
