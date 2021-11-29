2718231113e53c6260c2b7e2a2de0fbdb3891cb7
snowman <unmem@outlook.com>
2021-11-29 09:48:18 +0800

    matplotlib_examples: udpate settings.py (set ROBOTSTXT_OBEY to false)

--- matplotlib_examples/matplotlib_examples/settings.py
+++ matplotlib_examples/matplotlib_examples/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'matplotlib_examples.spiders'
 #USER_AGENT = 'matplotlib_examples (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
